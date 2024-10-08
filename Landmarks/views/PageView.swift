28a896a9ed069d3f595359752970388aff998319
Tob <tobadoba.1@gmail.com>
2024-10-08 18:12:49 +0000

    Feature page

---
+++ Landmarks/views/PageView.swift
@@ -0,0 +1,28 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Tob Adoba on 10/8/24.
+//
+
+import SwiftUI
+
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+
+
+    var body: some View {
+        VStack {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            Text("Current Page: \(currentPage)")
+        }
+        .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+
+#Preview {
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+}
