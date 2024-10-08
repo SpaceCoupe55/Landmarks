cfce8e8c3c7a7c7301f6cf85b045c3777ca6206b
Tob <tobadoba.1@gmail.com>
2024-10-08 17:38:37 +0000

    Working with UI controls

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,29 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Tob Adoba on 10/8/24.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "Tob Adoba")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
