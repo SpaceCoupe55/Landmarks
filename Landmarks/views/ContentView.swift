a81dedd8de988560ea044080b66f8228b732246f
Tob <tobadoba.1@gmail.com>
2024-10-08 14:04:59 +0000

    first commit

---
+++ Landmarks/views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Tob Adoba on 9/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
+    
+    var body: some View {
+        
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem{
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem{
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
